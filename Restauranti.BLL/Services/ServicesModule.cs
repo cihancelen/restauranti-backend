23507c48e9d27371c2d1cb3a1a0e968670e78367
Cihan Çelen <cihan.celen37@hotmail.com>
2019-11-11 01:34:16 +0300

    Servis ve repo katmanında yazılanlar modullere eklendi.

--- Restauranti.BLL/Services/ServicesModule.cs
+++ Restauranti.BLL/Services/ServicesModule.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.Extensions.DependencyInjection;
+using Restauranti.BLL.Services.Abstract.Category;
 using Restauranti.BLL.Services.Abstract.Restaurant;
+using Restauranti.BLL.Services.Concrete.Category;
 using Restauranti.BLL.Services.Concrete.Restaurant;
 
 namespace Restauranti.BLL.Services
@@ -10,6 +12,8 @@ namespace Restauranti.BLL.Services
         public ServicesModule(IServiceCollection services)
         {
             services.AddScoped<IRestaurantService, RestaurantService>();
+
+            services.AddScoped<ICategoryService, CategoryService>();
         }
     }
 }
--- Restauranti.DAL/Repositories/RepositoriesModule.cs
+++ Restauranti.DAL/Repositories/RepositoriesModule.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using Microsoft.Extensions.DependencyInjection;
 using Restauranti.DAL.Repositories.Abstract;
+using Restauranti.DAL.Repositories.Abstract.Category;
 using Restauranti.DAL.Repositories.Abstract.Restaurant;
 using Restauranti.DAL.Repositories.Concrete;
+using Restauranti.DAL.Repositories.Concrete.Category;
 using Restauranti.DAL.Repositories.Concrete.Restaurant;
 
 namespace Restauranti.DAL.Repositories
@@ -14,6 +16,8 @@ namespace Restauranti.DAL.Repositories
             services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
 
             services.AddScoped<IRestaurantRepository, RestaurantRepository>();
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
         }
     }
 }
